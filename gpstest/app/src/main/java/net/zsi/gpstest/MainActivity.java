41730be9be9c99d39c1d0981de8cf0b073373ba4
smager <gm_fuentes@yahoo.com>
2020-10-12 15:27:06 +0800

    gps test

---
+++ gpstest/app/src/main/java/net/zsi/gpstest/MainActivity.java
@@ -0,0 +1,236 @@
+package net.zsi.gpstest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int DEFAULT_UPDATE_INTERVAL = 5;
+    public static final int FAST_UPDATE_INTERVAL = 5;
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
+    private static final String TAG = "MainActivity";
+    int ctr =0,ctr2 =0;
+    float distance=0;
+    Location lastLocation;
+
+    TextView tv_lat, tv_lon, tv_altitude,tv_accuracy, tv_speed,tv_distance, tv_sensor,tv_updates,tv_address;
+    Button btn_distance;
+
+    Switch sw_locationupdates,sw_gps;
+    boolean updateOn = false;
+
+    LocationRequest locationRequest;
+    LocationCallback locationCallBack;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_distance = findViewById(R.id.tv_distance);
+
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        tv_address = findViewById(R.id.tv_address);
+
+        sw_locationupdates = findViewById(R.id.sw_locationsupdates);
+        sw_gps = findViewById(R.id.sw_gps);
+        btn_distance = findViewById(R.id.btn_distance);
+
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
+        locationRequest.setPriority(locationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
+        btn_distance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                distance=0;
+            }
+        });
+
+        locationCallBack = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+
+                super.onLocationResult(locationResult);
+                updateUIValues(locationResult.getLastLocation());
+            }
+        };
+
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_gps.isChecked()){
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Using GPS sensors");
+                }else{
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("Using Towers + WIFI");
+                }
+            }
+        });
+
+        sw_locationupdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_locationupdates.isChecked()){
+                    updateGPS();
+                    startLocationUpdates();
+
+                }else{
+
+                    stopLocationUpdates();
+                }
+
+
+            }
+        });
+
+
+
+
+    }// end onCreate method
+
+    private void stopLocationUpdates() {
+        tv_updates.setText("Location is NOT being tracked");
+        tv_lat.setText("Not tracking location");
+        tv_lon.setText("Not tracking location");
+        tv_speed.setText("Not tracking location");
+        tv_distance.setText("Not tracking location");
+        tv_address.setText("Not tracking location");
+        tv_accuracy.setText("Not tracking location");
+        tv_altitude.setText("Not tracking location");
+        tv_sensor.setText("Not tracking location");
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    private void startLocationUpdates() {
+        tv_updates.setText("Location is being tracked");
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallBack,null);
+        updateGPS();
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG,"onRequestPermissionsResult, requestCode:" + requestCode);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    updateGPS();
+                }else{
+
+                    Toast.makeText(this,"This app requires permission to be granted in order to work properly",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+
+        }
+    }
+
+    private void updateGPS(){
+        ctr2 +=1;
+        Log.d(TAG,"updateGPS, ctr:" + ctr2);
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if(
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                   //     && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.d(TAG,"updateGPS.onSuccess");
+                    updateUIValues(location);
+                }
+            });
+
+        }
+        else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_FINE_LOCATION);
+            }
+
+
+        }
+
+
+    }
+
+    private void updateUIValues(Location location) {
+
+        if(lastLocation != null) {
+            if(location.getSpeed() > 0) distance += lastLocation.distanceTo(location);
+        }
+
+        Log.d(TAG,"Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude() + ", distance(m):" +  distance);
+
+        tv_lat.setText(String.valueOf(location.getLatitude()) );
+        tv_lon.setText(String.valueOf(location.getLongitude()) );
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()) );
+        tv_distance.setText(String.valueOf(distance) );
+
+        if(location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()) );
+        }else{
+            tv_altitude.setText("Not available");
+        }
+
+        if(location.hasSpeed()){
+            tv_speed.setText(String.valueOf(location.getSpeed()) );
+        }else{
+            tv_speed.setText("Not available");
+        }
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+        try{
+            List<Address> addresses =  geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            tv_address.setText(addresses.get(0).getAddressLine(0));
+
+        }
+        catch (Exception e){
+            tv_address.setText("Unable to get street address");
+        }
+
+        lastLocation=location;
+
+    }
+
+
+}
